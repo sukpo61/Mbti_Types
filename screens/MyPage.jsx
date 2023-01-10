ac56e621ad8be27ac6842dd19a2aa49d91c2c766
u_ju <89282724+Sangwoo950@users.noreply.github.com>
2023-01-10 10:43:46 +0900

    [feat] mycommunity

--- screens/MyPage.jsx
+++ screens/MyPage.jsx
@@ -1,6 +1,30 @@
 import React from "react";
-import { Text } from "react-native";
+import styled from "styled-components/native";
 
 export default function MyPage() {
-  return <Text>MyPage</Text>;
+  return (
+    <>
+      <MyPageTitleWrap>
+        <Text>닉네임님의 MBTI는</Text>
+        <Text>INFP입니다.</Text>
+      </MyPageTitleWrap>
+      <LogoutBox>
+        <Text>로그아웃</Text>
+      </LogoutBox>
+    </>
+  );
 }
+
+const MyPageTitleWrap = styled.View`
+  background-color: #efe8fa;
+  height: 112px;
+  padding: 40px 32px;
+  justify-content: center;
+`;
+
+const Text = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const LogoutBox = styled.View``;
