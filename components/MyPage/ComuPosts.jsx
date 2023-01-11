3814913ae0cfacba7ae40af5d1e1078a72ba4e0d
go <sukpo6010@gmail.com>
2023-01-11 14:21:46 +0900

    feat: 선택 스타일

---
+++ components/MyPage/ComuPosts.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function ComuPosts() {
+  return <Text>ComuPosts</Text>;
+}
--- screens/MyPage.jsx
+++ screens/MyPage.jsx
@@ -4,7 +4,10 @@ import styled from "@emotion/native";
 import { useFocusEffect } from "@react-navigation/native";
 // import { GREEN_COLOR, YELLOW_COLOR } from "../colors";
 
-import { authService } from "../firebase";
+import { authService, dbService } from "../firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import ComuPosts from "../components/MyPage/ComuPosts";
+import QnAPosts from "../components/MyPage/QnAPosts";
 
 // import {
 //   collection,
@@ -18,6 +21,8 @@ import { authService } from "../firebase";
 import { signOut } from "firebase/auth";
 
 export default function My({ navigation: { navigate, reset, setOptions } }) {
+  const [postlist, setPostlist] = useState([]);
+  const [pagestate, setPagestate] = useState(true);
   const logout = () => {
     signOut(authService)
       .then(() => {
@@ -67,27 +72,72 @@ export default function My({ navigation: { navigate, reset, setOptions } }) {
       });
     }, [])
   );
+
+  const ComuText = styled.Text`
+    color: ${pagestate ? "#312070" : ""};
+  `;
+  const LikeText = styled.Text`
+    color: ${pagestate ? "" : "#312070"};
+  `;
   return (
     <>
       <MyPageTitleWrap>
-        <Text>닉네임님의 MBTI는</Text>
-        <Text>INFP입니다.</Text>
+        <Text>{authService.currentUser?.displayName}님의 MBTI는</Text>
+        <Text>{authService.currentUser?.photoURL}입니다.</Text>
       </MyPageTitleWrap>
-      <LogoutBox>
-        <Text>로그아웃</Text>
-      </LogoutBox>
+      <PostNav>
+        <ComuNav
+          onPress={() => {
+            setPagestate(true);
+          }}
+        >
+          <ComuText>내가 작성한글</ComuText>
+        </ComuNav>
+        <LikeNav
+          onPress={() => {
+            setPagestate(false);
+          }}
+        >
+          <LikeText>내가 좋아요 한 글</LikeText>
+        </LikeNav>
+      </PostNav>
+      {pagestate ? <ComuPosts></ComuPosts> : <QnAPosts></QnAPosts>}
     </>
   );
 }
 
+const Text = styled.Text``;
+
 const MyPageTitleWrap = styled.View`
+  display: flex;
+  flex-direction: column;
   background-color: #efe8fa;
   height: 112px;
-  padding: 40px 32px;
-  justify-content: center;
+  padding: 32px;
+  justify-content: space-around;
 `;
 
-const Text = styled.Text``;
+const PostNav = styled.View`
+  background-color: whitesmoke;
+  height: 40px;
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+`;
+
+const ComuNav = styled.TouchableOpacity`
+  height: 40px;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LikeNav = styled.TouchableOpacity`
+  height: 40px;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+`;
 
 const LogoutBox = styled.View``;
 
